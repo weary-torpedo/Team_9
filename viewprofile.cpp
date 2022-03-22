48fffb37ec0be0081032a8dc86f9377474e44184
pmlinh21 <pmlinh21@clc.fitus.edu.vn>
2022-03-22 18:52:53 +0700

    staffCreate

--- viewprofile.cpp
+++ viewprofile.cpp
@@ -61,6 +61,84 @@ void gotoxy(int x, int y)
     SetConsoleCursorPosition (GetStdHandle(STD_OUTPUT_HANDLE), c);
 } 
 
+void box(int x, int y, int w, int h){
+	for(int i = x; i <= x + w; i++){
+		gotoxy(i, y);
+		cout << char(196);
+		gotoxy(i, y + h);
+		cout << char(196);
+	}
+	for(int i = y; i <= y + h; i++){
+		gotoxy(x, i);
+		cout << char(179);
+		gotoxy(x + w, i);
+		cout << char(179);
+	}
+	gotoxy(x, y); cout << char(218);
+	gotoxy(x, y + h); cout << char(192);
+	gotoxy(x + w, y); cout << char(191);
+	gotoxy(x + w, y + h); cout << char(217);
+}
+
+bool loginCheck(string username, string password){
+	ifstream fin;
+	if(username[0] >= '0' && username[0] <= '9'){
+		fin.open("loginStu.txt");
+		string userCheck, passCheck;
+		while(!fin.eof()){
+			fin >> userCheck >> passCheck;
+			if(username == userCheck && password == passCheck)
+				return true;
+		}
+		return false;
+	}
+	else if(username[0] == 's'){
+			fin.open("loginStaff.txt");
+			string userCheck, passCheck;
+			while(!fin.eof()){
+				fin >> userCheck >> passCheck;
+				if(username == userCheck && password == passCheck)
+					return true;
+			}
+			return false;
+		 }
+		 else return false;
+}
+
+string loginSystem(){
+	system("cls");
+	string username, password;
+	gotoxy(41,11);
+	cout << "USERNAME:";
+	box(50,10,25,2);
+	gotoxy(41,14);
+	cout << "PASSWORD:";
+	box(50,13,25,2);
+	int again = 0;
+	while(again < 5){
+		gotoxy(52,11);
+		cin >> username;
+		gotoxy(52,14);
+		cin >> password;
+		if(loginCheck(username,password)){
+			return username;
+		}
+		else{
+			again++;
+			gotoxy(52,11);
+			for(int i = 1; i <= username.length(); i++)
+				cout << " ";
+			gotoxy(52,14);
+			for(int i = 1; i <= password.length(); i++)
+				cout << " ";
+			gotoxy(48,17);
+			cout << "INVALID USERNAME OR PASSWORD. PLEASE TRY AGAIN.";	
+		}
+	}
+	username = "invalid";
+	return username;
+}
+
 void printBox(string text, int x, int y, int size)
 {	
 	gotoxy(x,y);
@@ -115,12 +193,19 @@ void createCourseRegister(Year *&pCurYear, int n)
 //	}
 }
 
-void staffCreate(Year *&pYear, Year *&pCurYear)
-{
+void createSemester(Year *&pCurYear, int &orderSem){
+}
+
+void createClass(Year *&pCurYear){
+}
+
+void createYear(Year *&pCurYear){
+}
+
+void staffCreate(Year *&pYear, Year *&pCurYear){
 	pCurYear = pYear;
-	int n;
-	while (true)
-	{
+	int orderSem;
+	while (!logOut){
 		system("cls");
 		printBox("Press 1 to create a school year",xp,5,50);
 		printBox("Press 2 to create a class",xp,8,50);
@@ -129,18 +214,21 @@ void staffCreate(Year *&pYear, Year *&pCurYear)
 		printBox("Press O to log out",xp,17,50);
 		
 		char c = getch();
-		if (c == 'o' || c == 'O')
-			exit(0);
-//	else if ( c == '1')
-//			createYear(pCurYear);
-//	else if ( c == '2')
-//			createClass(pCurYear);
-//	else if ( c == '3')
-//		createSemester(pCurYear,n);
+		if (c == 'o' || c == 'O'){
+			logOut = true;
+			return;
+		}
+	else if ( c == '1')
+			createYear(pCurYear);
+	else if ( c == '2')
+			createClass(pCurYear);
+	else if ( c == '3')
+			createSemester(pCurYear,orderSem);
 	else if ( c == '4')
-			createCourseRegister(pCurYear, n);		
+			createCourseRegister(pCurYear, orderSem);		
 	}
 }
+
 void changePassword(){	
 }
 
@@ -179,7 +267,7 @@ void viewProfile(Student *pStu){
 	getch();
 }
 
-void homePage(Student *pStu){
+void homePage(Student *pStu, string username){
 	while (true){
 		system("cls");
 		if ('2' <= username[0] && username[0] <= '9')
@@ -227,4 +315,6 @@ int main(){
 	
 	gotoxy(30,30);
 	return 0;
+}
+
 }
